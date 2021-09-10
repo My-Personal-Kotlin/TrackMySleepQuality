39a27f8dd5078cd62a4970a081f8e81b4d0df4bd
Awais Mansha <awaismansha1998@gmail.com>
2021-09-10 23:56:23 +0500

    WOrking on Lesson 6 in Kotlin

--- app/src/main/java/com/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -1,3 +1,33 @@
 package com.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SleepDatabaseDao{
+
+    @Insert
+    fun insert(sleepNight: SleepNight)
+
+    @Update
+    fun update(sleepNight: SleepNight)
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE night_id=:key")
+    fun get(key: Long): SleepNight?
+
+    @Query("DELETE FROm daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY night_id DESC")
+    fun getAllNights():LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY night_id DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+
+    // Extra
+    @Delete
+    fun delete(night: SleepNight)
+
+    @Delete
+    fun deleteAllNights(nights: List<SleepNight>): Int
+}
